81a1516792c17d1d37519f58fa4963b4c0d33a6c
dusartc <dusart.clement@gmail.com>
2017-07-28 13:32:46 +0200

    bouton qui proc la camera

--- app/src/main/java/adonif/adonifmobile/MainActivity.java
+++ app/src/main/java/adonif/adonifmobile/MainActivity.java
@@ -1,7 +1,10 @@
 package adonif.adonifmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
     }
 }
